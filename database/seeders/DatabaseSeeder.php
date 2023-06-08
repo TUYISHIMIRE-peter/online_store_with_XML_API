b48c5d859511f3b6dd71c6f33fc1d9dd1c3e5f94
ifaustin89 <jamesofficial31@gmail.com>
2023-06-09 01:02:34 +0200

    tested API with POSTMAN

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'ELECTRONIC'
+        ]);
+        Category::create([
+            'name' => 'FURNITURE'
+        ]);
+        Category::create([
+            'name' => 'clothes'
+        ]);
+        Category::create([
+            'name' => 'electricity'
+        ]);
+    }
+}
